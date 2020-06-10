0fb3a8bcf8369ef8c34e667898df863a8210e4c1
tajulien <qvjulienta@gmail.com>
2020-06-10 08:32:25 +0200

    WiP C# N- tank

--- C#/Program.cs
+++ C#/Program.cs
@@ -15,17 +15,17 @@ namespace ConsoleApplication1
             List<int> containers_volume = new List<int>();
             List<int> starting_node = new List<int>();
 
-            int vol_wanted = 5;
-            int containers_count = 3;
+            int vol_wanted = 4;
+            int containers_count = 2;
 
             int i;
             starting_node.Add(0);
             starting_node.Add(0);
-            starting_node.Add(0);
+            //starting_node.Add(0);
 
-            containers_volume.Add(7);
-            containers_volume.Add(9);
-            containers_volume.Add(13);
+            containers_volume.Add(3);
+            containers_volume.Add(5);
+            //containers_volume.Add(13);
 
             //for (i = 0; i < containers_count; i++)
             //{
@@ -40,10 +40,10 @@ namespace ConsoleApplication1
 
         public static void search(List<int> starting_node, List<int> containers_volume, int vol_wanted, Dictionary<double, bool> check_dict, int containers_count)
         {
-            List<Object> target = new List<Object>();
+            List<List<int>> target = new List<List<int>>();
             List<List<int>> q = new List<List<int>>();
             List<List<int>> path = new List<List<int>>();
-            List<Object> next_moves;
+            List<List<int>> next_moves;
             bool accomplished = false;
 
             q.Add(starting_node);
@@ -51,29 +51,34 @@ namespace ConsoleApplication1
 
             while (q.Count != 0)
             {
+                //path_2 = q[0];
                 List<int> path_2 = new List<int>();
-                path_2 = q[0];
                 List<int> comp = new List<int>();
+                List<List<int>> ee = new List<List<int>>();
+                List<List<int>> eb = new List<List<int>>();
+
                 for (int i = 0; i < containers_count; i++)
                     comp.Add(0);
-                if (path_2 == comp)
+                
+                if (q[0] == comp)
                 {
-                    path.Add(new ArrayList(path_2));
+                    ee.Add(q[0]);
+                    path = ee;
                 }
                 else {
-                    path = (List<Object>)(path_2);
+                    path_2 = new List<int>(q[0]);
+                    eb.Add(path_2);
+                    path = new List<List<int>>(eb);
                 }
-                q.Remove(0);
+
+                q.RemoveAt(0);
 
                 List<int> last_path;
-                Console.WriteLine(path.ToString());
                 int zef = 0;
                 zef = ((int)path.Count) - 1;
-                last_path = (List<int>) path[zef];
-                Console.WriteLine(last_path);
+                last_path = path[zef];
                 double b = get_index(last_path, containers_count);
-                check_dict.Add(b, true);
-
+                check_dict[b] = true;
                 // If target is reached, exit the loop
                 if (is_vol_wanted(path, vol_wanted, containers_count))
                 {
@@ -89,8 +94,8 @@ namespace ConsoleApplication1
             // If accomplished, print the path
             if (accomplished)
             {
+                Console.WriteLine("Accomplished");
                 print_path(target);
-                //System.out.println(target);
             }
             else
             {
@@ -110,7 +115,7 @@ namespace ConsoleApplication1
         // Finding the next path with checking the ones already checked
         {
             List<List<int>> result = new List<List<int>>();
-            ArrayList next_nodes = new ArrayList();
+            List<List<int>> next_nodes = new List<List<int>>();
             List<int> last_path;
             List<int> max_containers_volume = new List<int>();
             List<int> containers_status = new List<int>();
@@ -120,7 +125,7 @@ namespace ConsoleApplication1
                 max_containers_volume.Add(containers_volume[i]);
 
 
-            last_path = (List<int>) path[(path.Count - 1)];
+            last_path = path[(path.Count - 1)];
             for (int i = 0; i < containers_count; i++)
                 containers_status.Add(last_path[i]);
 
@@ -132,7 +137,7 @@ namespace ConsoleApplication1
                     the_node.Add(containers_status[i]);
                 the_node[j] = max_containers_volume[j];
                 if (been_there(the_node, check_dict, containers_count))
-                    next_nodes.Add(new ArrayList(the_node));
+                    next_nodes.Add(new List<int>(the_node));
                 the_node.Clear();
             }
 
@@ -142,7 +147,7 @@ namespace ConsoleApplication1
                     the_node.Add(containers_status[i]);
                 the_node[j] = 0;
                 if (been_there(the_node, check_dict, containers_count))
-                    next_nodes.Add(new ArrayList(the_node));
+                    next_nodes.Add(new List<int>(the_node));
                 the_node.Clear();
             }
 
@@ -157,7 +162,7 @@ namespace ConsoleApplication1
                         the_node[i] = Math.Min((int) max_containers_volume[i], (int) the_node[i] + (int) the_node[j]);
                         the_node[j] = ((int)the_node[j] - ((int)the_node[i] - (int)containers_status[i]));
                         if (been_there(the_node, check_dict, containers_count))
-                            next_nodes.Add(new ArrayList(the_node));
+                            next_nodes.Add(new List<int>(the_node));
                         the_node.Clear();
                     }
                 }
@@ -165,15 +170,18 @@ namespace ConsoleApplication1
             }
 
             // Create the list of possible next path
-            foreach (List<List<int>> arrayList in next_nodes)
+            if (next_nodes.Count != 0)
             {
-                List<List<int>> temp;
-                temp = new List<List<int>>(path);
-                temp.Add(arrayList);
-                result.Add(new List<int>(temp));
-                temp.Clear();
+                foreach (List<int> arrayList in next_nodes)
+                {
+                    List<List<int>> temp;
+                    temp = new List<List<int>>(path);
+                    temp.Add(arrayList);
+                    foreach (List<int> addit in temp)
+                    result.Add(addit);
+                    temp.Clear();
+                }
             }
-
             return result;
 
         }
@@ -212,25 +220,28 @@ namespace ConsoleApplication1
         }
 
 
-        public static void print_path(List<Object> path)
+        public static void print_path(List<List<int>> path)
         {
+            Console.WriteLine(path.Sum(x => x.Count));
+
             Console.WriteLine("Solution in " + (path.Count - 1) + " steps");
+
             for (int i = 0; i < path.Count - 1; i++)
             {
-                write_transition((List<int>)path[i], (List<int>) path[i + 1]);
+                write_transition(path[i], path[i + 1]);
                 String step;
                 step = path[(i + 1)].ToString().Replace("\\[", " \\(").Replace("]", "\\)");
                Console.WriteLine(step);
             }
         }
 
-        public static bool is_vol_wanted(List<Object> path, int vol_wanted, int containers_count)
+        public static bool is_vol_wanted(List<List<int>> path, int vol_wanted, int containers_count)
         {
             //Checking the last node and return True if we reached the targeted value
 
             int temp4;
             List<int> last_path;
-            last_path = (List<int>) path[(path.Count - 1)];
+            last_path = path[(path.Count - 1)];
             for (int i = 0; i < containers_count; i++)
             {
                 temp4 = (int) last_path[i];
